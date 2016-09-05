292e35b1be26361677265bba63a5bd7972370017
kvago36 <kvago36@gmail.com>
2016-09-05 19:15:51 +0300

    data to controller

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
 var routes = require('./app_server/routes/index');
-//var users = require('./app_server/routes/users');
+// var users = require('./app_server/routes/users');
 
 var app = express();
 
@@ -22,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-//app.use('/users', users);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -54,5 +55,6 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
+
 
 module.exports = app;
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,14 +3,12 @@ var router = express.Router();
 var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
 
-
-
 /* Locations pages */
-router.get('/', ctrlLocations.homeList);
-router.get('/locations', ctrlLocations.locationInfo);
-router.get('/locations/review/new' , ctrlLocations.addReview);
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
 
 /* Other pages */
-router.get('/about' , ctrlOthers.about);
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
